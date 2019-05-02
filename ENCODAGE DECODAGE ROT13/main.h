dd6887d5e8ba9129420104e6117c8c2e9c39020c
umignon <ugo.mignon@tartanrunstudio.com>
2019-05-02 16:33:00 +0200

    template

--- ENCODAGE DECODAGE ROT13/main.h
+++
@@ -1,5 +0,0 @@
-char charEncode(char Char);
-char* stringEncode(char* string);
-void arg(char* argv[]);
-void handle(char* path);
-const char* get_ext(const char* filename);
---
+++ src/main/main.h
@@ -0,0 +1,5 @@
+char charEncode(char Char);
+char* stringEncode(char* string);
+void arg(char* argv[]);
+void handle(char* path);
+const char* get_ext(const char* filename);
